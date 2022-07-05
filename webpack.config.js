2decc44bac2cf4173a13652e6e4f1c5e2fcac918
chicco4 <chiccotto4@gmail.com>
2022-07-05 12:24:01 +0200

    sourcemap util for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devtool: 'source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'to-do-list',
