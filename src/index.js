601802e7e2fd01fc2d074a118ccc93175911b7fc
chicco4 <chiccotto4@gmail.com>
2022-07-08 12:12:35 +0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,150 +1,138 @@
-import './styles/global.css'
-import { format } from 'date-fns'
-import Storage from './modules/Storage.js'
-import Project from './modules/Project.js'
-import Task from './modules/Task.js'
+import "./styles/global.css";
+import { format } from "date-fns";
+import Storage from "./modules/Storage.js";
+import Project from "./modules/Project.js";
+import Task from "./modules/Task.js";
 
-import diamondIMP from './assets/diamond_FILL0_wght400_GRAD0_opsz48.svg'
-import inboxIMP from './assets/inbox_FILL0_wght400_GRAD0_opsz48.svg'
-import todayIMP from './assets/today_FILL0_wght400_GRAD0_opsz48.svg'
-import thisWeekIMP from './assets/date_range_FILL0_wght400_GRAD0_opsz48.svg'
-import addIMP from './assets/add_FILL0_wght400_GRAD0_opsz48.svg'
-import circleIMP from './assets/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg'
-import cirleCheckIMP from './assets/check_circle_FILL0_wght400_GRAD0_opsz48.svg'
-import closeIMP from './assets/close_FILL0_wght400_GRAD0_opsz48.svg'
-import projIMP from './assets/checklist_FILL0_wght400_GRAD0_opsz48.svg'
+import diamondIMP from "./assets/diamond_FILL0_wght400_GRAD0_opsz48.svg";
+import inboxIMP from "./assets/inbox_FILL0_wght400_GRAD0_opsz48.svg";
+import todayIMP from "./assets/today_FILL0_wght400_GRAD0_opsz48.svg";
+import thisWeekIMP from "./assets/date_range_FILL0_wght400_GRAD0_opsz48.svg";
+import addIMP from "./assets/add_FILL0_wght400_GRAD0_opsz48.svg";
+import circleIMP from "./assets/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg";
+import cirleCheckIMP from "./assets/check_circle_FILL0_wght400_GRAD0_opsz48.svg";
+import closeIMP from "./assets/close_FILL0_wght400_GRAD0_opsz48.svg";
+import projIMP from "./assets/checklist_FILL0_wght400_GRAD0_opsz48.svg";
 
 /* HEADER */
 
 function createHeader() {
-    const header = document.createElement("header");
+  const header = document.createElement("header");
 
-    const diamondImg = document.createElement("img");
-    diamondImg.src = diamondIMP
+  const diamondImg = document.createElement("img");
+  diamondImg.src = diamondIMP;
 
-    const pageTitle = document.createElement("h1");
-    pageTitle.textContent = "To Do List";
+  const pageTitle = document.createElement("h1");
+  pageTitle.textContent = "To Do List";
 
-    header.appendChild(diamondImg);
-    header.appendChild(pageTitle);
+  header.appendChild(diamondImg);
+  header.appendChild(pageTitle);
 
-    return header;
+  return header;
 }
 
 /* MAIN */
 
 function createMain() {
+  /* nav */
 
-    /* nav */
+  const main = document.createElement("main");
 
-    const main = document.createElement("main")
+  const nav = document.createElement("nav");
 
-    const nav = document.createElement("nav")
+  nav.appendChild(createButton("Inbox"));
+  nav.appendChild(createButton("Today"));
+  nav.appendChild(createButton("This week"));
 
-    nav.appendChild(createButton("Inbox"))
-    nav.appendChild(createButton("Today"))
-    nav.appendChild(createButton("This week"))
+  const title = document.createElement("h1");
+  title.textContent = "Projects";
 
-    const title = document.createElement("h1")
-    title.textContent = "Projects"
+  const addProjectButton = document.createElement("buttons");
+  addProjectButton.textContent = "Projects";
 
-    const addProjectButton = document.createElement("buttons")
-    addProjectButton.textContent = "Projects"
+  nav.appendChild(title);
+  nav.appendChild(getProjectsList());
+  nav.appendChild(createButton("Add Project"));
 
-    nav.appendChild(title)
-    nav.appendChild(getProjectsList())
-    nav.appendChild(createButton("Add Project"))
+  /* article */
 
-    /* article */
+  const article = document.createElement("article");
+  article.textContent = "article";
 
-    const article = document.createElement("article")
-    article.textContent = "article"
+  main.appendChild(nav);
+  main.appendChild(article);
 
-
-    main.appendChild(nav)
-    main.appendChild(article)
-
-    return (main);
+  return main;
 }
 
 function getProjectsList() {
+  const projList = document.createElement("div");
+  projList.classList.add("projects-list");
 
-
-
-
-    const projList = document.createElement("div")
-    projList.classList.add("projects-list")
-
-    return projList
+  return projList;
 }
 
 function createButton(string) {
+  const button = document.createElement("button");
+  const inboxImg = document.createElement("img");
 
-    const button = document.createElement("button")
-    const inboxImg = document.createElement("img")
+  let imp;
+  switch (string) {
+    case "Inbox":
+      imp = inboxIMP;
+      break;
+    case "Today":
+      imp = todayIMP;
+      break;
+    case "This week":
+      imp = thisWeekIMP;
+      break;
+    case "Add Project":
+      imp = addIMP;
+      break;
+  }
 
-    let imp;
-    switch (string) {
-        case 'Inbox':
-            imp = inboxIMP;
-            break;
-        case 'Today':
-            imp = todayIMP;
-            break;
-        case 'This week':
-            imp = thisWeekIMP;
-            break;
-        case 'Add Project':
-            imp = addIMP;
-            break;
-    }
+  inboxImg.src = imp;
+  const buttonTitle = document.createElement("p");
+  buttonTitle.textContent = string;
 
-    inboxImg.src = imp;
-    const buttonTitle = document.createElement("p")
-    buttonTitle.textContent = string
+  button.appendChild(inboxImg);
+  button.appendChild(buttonTitle);
 
-    button.appendChild(inboxImg)
-    button.appendChild(buttonTitle)
+  button.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
 
-    button.addEventListener("click", (e) => {
-        if (e.target.classList.contains("active")) return;
+    console.log(string);
+  });
 
-        console.log(string)
-    })
-
-    return button;
+  return button;
 }
-
-
-
 
 /* FOOTER */
 
 function createFooter() {
-    const footer = document.createElement("footer");
-    footer.classList.add("footer");
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
 
-    const copyright = document.createElement("p");
-    copyright.textContent = `Copyright © ${new Date().getFullYear()}`;
+  const copyright = document.createElement("p");
+  copyright.textContent = `Copyright © ${new Date().getFullYear()}`;
 
-    const githubLink = document.createElement("a");
-    githubLink.href = "https://github.com/chicco4";
-    githubLink.textContent = "chicco4"
+  const githubLink = document.createElement("a");
+  githubLink.href = "https://github.com/chicco4";
+  githubLink.textContent = "chicco4";
 
-    footer.appendChild(copyright);
-    footer.appendChild(githubLink);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
 
-    return footer;
+  return footer;
 }
 
 /* START */
 
 function initializeContent() {
-
-    document.body.appendChild(createHeader());
-    document.body.appendChild(createMain());
-    document.body.appendChild(createFooter());
-
+  document.body.appendChild(createHeader());
+  document.body.appendChild(createMain());
+  document.body.appendChild(createFooter());
 }
 
 initializeContent();
--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,55 +1,55 @@
-import { toDate, isToday, isThisWeek, subDays } from 'date-fns'
+import { toDate, isToday, isThisWeek, subDays } from "date-fns";
 
 export default class Project {
-    constructor(name) {
-        this.name = name
-        this.tasks = []
-    }
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
 
-    setName(name) {
-        this.name = name
-    }
+  setName(name) {
+    this.name = name;
+  }
 
-    getName() {
-        return this.name
-    }
+  getName() {
+    return this.name;
+  }
 
-    setTasks(tasks) {
-        this.tasks = tasks
-    }
+  setTasks(tasks) {
+    this.tasks = tasks;
+  }
 
-    getTasks() {
-        return this.tasks
-    }
+  getTasks() {
+    return this.tasks;
+  }
 
-    getTask(taskName) {
-        return this.tasks.find((task) => task.getName() === taskName)
-    }
+  getTask(taskName) {
+    return this.tasks.find((task) => task.getName() === taskName);
+  }
 
-    contains(taskName) {
-        return this.tasks.some((task) => task.getName() === taskName)
-    }
+  contains(taskName) {
+    return this.tasks.some((task) => task.getName() === taskName);
+  }
 
-    addTask(newTask) {
-        if (this.tasks.find((task) => task.getName() === newTask.name)) return
-        this.tasks.push(newTask)
-    }
+  addTask(newTask) {
+    if (this.tasks.find((task) => task.getName() === newTask.name)) return;
+    this.tasks.push(newTask);
+  }
 
-    deleteTask(taskName) {
-        this.tasks = this.tasks.filter((task) => task.name !== taskName)
-    }
+  deleteTask(taskName) {
+    this.tasks = this.tasks.filter((task) => task.name !== taskName);
+  }
 
-    getTasksToday() {
-        return this.tasks.filter((task) => {
-            const taskDate = new Date(task.getDateFormatted())
-            return isToday(toDate(taskDate))
-        })
-    }
+  getTasksToday() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isToday(toDate(taskDate));
+    });
+  }
 
-    getTasksThisWeek() {
-        return this.tasks.filter((task) => {
-            const taskDate = new Date(task.getDateFormatted())
-            return isThisWeek(subDays(toDate(taskDate), 1))
-        })
-    }
+  getTasksThisWeek() {
+    return this.tasks.filter((task) => {
+      const taskDate = new Date(task.getDateFormatted());
+      return isThisWeek(subDays(toDate(taskDate), 1));
+    });
+  }
 }
